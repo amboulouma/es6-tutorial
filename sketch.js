a79165a4e423fe7000baca85b13739f14e0e61e0
flambok <amine@flambok.com>
2017-12-05 04:22:37 +0100

    Commit ES6 example with object oriented programming example

--- sketch.js
+++ sketch.js
@@ -1,26 +1,39 @@
-var bubble = {
-    x: 300,
-    y: 200
-}
+let bubble1;
+let bubble2;
 
 function setup() {
     createCanvas(600, 400);
+    bubble1 = new Bubble(200, 200, 40);
+    bubble2 = new Bubble(400, 200, 20);
 }
 
 function draw() {
     background(0);
-    move();
-    display();
+    bubble1.move();
+    bubble1.show();
+    bubble2.move();
+    bubble2.show();
 }
 
-function display() {
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    ellipse(bubble.x, bubble.y, 24, 24);
+class Bubble {
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, this.r*2);
+    }
+
+    move(){
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+
 }
 
-function move(){
-    bubble.x = bubble.x + random(-5, 5);
-    bubble.y = bubble.y + random(-5, 5);
-}
